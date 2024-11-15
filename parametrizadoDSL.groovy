53e6a7e2374304687acd179d07e8e6fd3cca31a4
Alejandro Cardona Gaviria <51707232+alejocardona@users.noreply.github.com>
2024-11-14 21:24:30 -0500

    Update parametrizadoDSL.groovy

--- parametrizadoDSL.groovy
+++ parametrizadoDSL.groovy
@@ -2,8 +2,8 @@ job('ejemplo2-job-DSL') {
 	description('Job DSL de ejemplo para el curso de Jenkins')
   	scm {
       		git('https://github.com/alejocardona/jenkins.job.parametrizado.git', 'main') { node ->
-        		node / gitConfigName('macloujulian')
-        		node / gitConfigEmail('macloujulian@gmail.com')
+        		node / gitConfigName('alejocardona')
+        		node / gitConfigEmail('cardalex@hotmail.com')
       		}
     	} 
   	parameters {
@@ -16,25 +16,5 @@ job('ejemplo2-job-DSL') {
     	}
   	steps {
     		shell("bash jobscript.sh")
-    	}
-  	publishers {
-      		mailer('macloujulian@gmail.com', true, true)
-      		slackNotifier {
-		  notifyAborted(true)
-		  notifyEveryFailure(true)
-		  notifyNotBuilt(false)
-		  notifyUnstable(false)
-		  notifyBackToNormal(true)
-		  notifySuccess(false)
-		  notifyRepeatedFailure(false)
-		  startNotification(false)
-		  includeTestSummary(false)
-		  includeCustomMessage(false)
-		  customMessage(null)
-		  sendAs(null)
-		  commitInfoChoice('NONE')
-		  teamDomain(null)
-		  authToken(null)
-        	}
     	}
 }
